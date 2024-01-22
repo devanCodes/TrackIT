be1afa455e82bc17899fdb2de69549786968b543
devanCodes <devanghailey@gmail.com>
2024-01-22 15:23:05 -0400

    AddFood input page styling

--- src/pages/subpages/inputpages/AddFood.jsx
+++ src/pages/subpages/inputpages/AddFood.jsx
@@ -1,37 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text, Box, Input } from "@chakra-ui/react";
 import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
 // THIS PAGE NEEDS TO CONDITIONALLY RENDER A BOX AFTER A OOD IS SELECTED
 // THAT INCLUDES THE AMOUNT AND UNITS
 // WORK WITH MYFITNESSPAL
+// HOOK UP API HERE
 
 const AddFood = () => {
   return (
-    <>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+    <Flex
+      w="100%"
+      minHeight="100vh"
+      direction="column"
+      backgroundColor="rgba(255,205,84,255)"
+      bgSize="cover"
+    >
       <Navbar />
-      <h1>This is the AddFood page</h1>
-      <h1>Add food to (meal name that will be dynamic)</h1>
-      <div>
-        Search Bar that uses API
-        <Button>Search</Button>
-      </div>
-      <Button>
-        <Link to="/tracking/diary">Cancel</Link>
-      </Button>
-      <Button>
+      <Heading
+        fontSize={["3xl", "3xl", "6xl"]}
+        fontWeight="extrabold"
+        pt={75}
+        mt="auto"
+        mx="auto"
+      >
+        Search and Add Food
+      </Heading>
+      <Text
+        fontSize={["md", "md", "lg"]}
+        fontWeight="semibold"
+        mx={100}
+        align="center"
+      >
+        Add food to (meal name that will be dynamic)
+      </Text>
+      <Box mx="auto" width="90%" align="center">
+        <Input variant="flushed" width="50%" mr={2}></Input>
+        <Button maxWidth={200}>
+          Search
+        </Button>
+      </Box>
+      <Box maxWidth={200} mx="auto" pt={5}>
         <Link to="/tracking/diary">
-          Add to (meal name that will be dynamic)
+          <Button mr={2}>Cancel</Button>
         </Link>
-      </Button>
+        <Link to="/tracking/diary">
+          <Button>Add Food</Button>
+        </Link>
+      </Box>
       <Footer />
-    </>
+    </Flex>
   );
 };
 
